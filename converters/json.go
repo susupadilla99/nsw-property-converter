7828522f18e13d7cf676f0210bcd12ada6d3ee20
Daniel Le <lehacongduy@gmail.com>
2025-03-09 22:41:23 +1100

    Refactored extractor to output struct data type instead of 2d string slice

--- converters/json.go
+++ converters/json.go
@@ -3,35 +3,11 @@ package converters
 import (
 	"encoding/json"
 	"fmt"
+
+	"github.com/susupadilla99/nsw-property-converter/extractors"
 )
 
-type Property struct {
-	Record_Type                string `json:"record_type"`
-	District_Code              string `json:"district_code"`
-	Property_ID                string `json:"property_id"`
-	Sale_Counter               string `json:"sale_counter"`
-	Download_Date_Time         string `json:"download_date_time"`
-	Property_Name              string `json:"property_name"`
-	Property_Unit_Number       string `json:"property_unit_number"`
-	Property_House_Number      string `json:"property_house_number"`
-	Property_Street_Name       string `json:"property_street_name"`
-	Property_Locality          string `json:"property_locality"`
-	Property_Post_Code         string `json:"property_post_code"`
-	Area                       string `json:"area"`
-	Area_Type                  string `json:"area_type"`
-	Contract_Date              string `json:"contract_date"`
-	Settlement_Date            string `json:"settlement_date"`
-	Purchase_Price             string `json:"purchase_price"`
-	Zoning                     string `json:"zoning"`
-	Nature_Of_Property         string `json:"nature_of_property"`
-	Primary_Purpose            string `json:"primary_purpose"`
-	Strata_Lot_Number          string `json:"strata_lot_number"`
-	Component_Code             string `json:"component_code"`
-	Sale_Code                  string `json:"sale_code"`
-	Percent_Interest_Of_Sale   string `json:"percent_interest_of_sale"`
-	Dealing_Number             string `json:"dealing_number"`
-	Property_Legal_Description string `json:"property_legal_description"`
-}
+type Property extractors.Property
 
 func ConvertSliceToJSON(data [][]string) string {
 	jsonData := []Property{}
